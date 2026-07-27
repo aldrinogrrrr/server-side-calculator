68ea28db25a0253e3f1a21d37bbb87bc13ac5fe9
Aldrin <137375592+aldrinogrrrr@users.noreply.github.com>
2026-07-27 10:34:58 +0800

    feat: refactored test specs and data

--- test-data/invalidInputData.js
+++ test-data/invalidInputData.js
@@ -1,8 +1,8 @@
 export const invalidInputCases = [
-    { description: 'first field left empty', numberOne: '', operator: 'plus', numberTwo: '5' },
-    { description: 'second field left empty', numberOne: '5', operator: 'plus', numberTwo: '' },
-    { description: 'both fields left empty', numberOne: '', operator: 'plus', numberTwo: '' },
-    { description: 'nonsense text instead of a number', numberOne: 'zzqxnonsense', operator: 'plus', numberTwo: '5' },
-    { description: 'a script tag, in case someone gets clever', numberOne: '<script>alert(1)</script>', operator: 'plus', numberTwo: '5' },
-    { description: 'whitespace pretending to be a value', numberOne: '   ', operator: 'plus', numberTwo: '5' },
+    { description: 'first field left empty', numberOne: '', operator: 'plus', numberTwo: '5', expected: 'ERR' },
+    { description: 'second field left empty', numberOne: '5', operator: 'plus', numberTwo: '', expected: 'ERR' },
+    { description: 'both fields left empty', numberOne: '', operator: 'plus', numberTwo: '', expected: 'ERR' },
+    { description: 'nonsense text instead of a number', numberOne: 'zzqxnonsense', operator: 'plus', numberTwo: '5', expected: 'ERR' },
+    { description: 'a script tag, in case someone gets clever', numberOne: '<script>alert(1)</script>', operator: 'plus', numberTwo: '5', expected: 'ERR' },
+    { description: 'whitespace pretending to be a value', numberOne: '   ', operator: 'plus', numberTwo: '5', expected: 'ERR' },
 ];
